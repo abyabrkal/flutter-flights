63a5b28ec9afec7991df674fb55cbbdac75e88a0
Aby Abraham <abyabrkal@gmail.com>
2019-04-16 10:24:36 +0400

    Error Cleared. Container having cityCards needs extra space to accomodate bottom price row.

--- lib/main.dart
+++ lib/main.dart
@@ -265,7 +265,7 @@ var homeScreenBottomPart = Column(
       ),
     ),
     Container(
-      height: 210,
+      height: 240,
       child: ListView(
         scrollDirection: Axis.horizontal,
         children: cityCards,
@@ -275,18 +275,17 @@ var homeScreenBottomPart = Column(
 );
 
 List<CityCard> cityCards = [
-  CityCard("assets/images/lasvegas.jpg", "Las Vegas", "Feb 2019", "45", "4299",
-      "2250"),
   CityCard(
-      "assets/images/athens.jpg", "Athens", "Apr 2019", "50", "9999", "4150"),
-  CityCard(
-      "assets/images/sydney.jpeg", "Sydney", "Mar 2019", "40", "5999", "2350"),
+      "assets/images/lasvegas.jpg", "Las Vegas", "Feb 2019", "45", 4299, 2250),
+  CityCard("assets/images/athens.jpg", "Athens", "Apr 2019", "50", 9999, 4159),
+  CityCard("assets/images/sydney.jpeg", "Sydney", "Dec 2018", "40", 5999, 2399)
 ];
 
 final formatCurrency = NumberFormat.simpleCurrency();
 
 class CityCard extends StatelessWidget {
-  final String imagePath, cityName, monthYear, discount, oldPrice, newPrice;
+  final String imagePath, cityName, monthYear, discount;
+  final int oldPrice, newPrice;
 
   CityCard(this.imagePath, this.cityName, this.monthYear, this.discount,
       this.oldPrice, this.newPrice);
@@ -294,42 +293,45 @@ class CityCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 8),
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
           ClipRRect(
-            borderRadius: BorderRadius.all(Radius.circular(12)),
+            borderRadius: BorderRadius.all(
+              Radius.circular(10.0),
+            ),
             child: Stack(
               children: <Widget>[
                 Container(
-                  height: 210,
-                  width: 160,
+                  height: 210.0,
+                  width: 160.0,
                   child: Image.asset(
                     imagePath,
                     fit: BoxFit.cover,
                   ),
                 ),
                 Positioned(
-                  left: 0,
-                  bottom: 0,
-                  width: 160,
-                  height: 60,
+                  left: 0.0,
+                  bottom: 0.0,
+                  width: 160.0,
+                  height: 60.0,
                   child: Container(
                     decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        begin: Alignment.bottomCenter,
-                        end: Alignment.topCenter,
-                        colors: [
-                          Colors.black, Colors.black.withOpacity(0.1),
-                        ]
-                      )
-                    ),
+                        gradient: LinearGradient(
+                            begin: Alignment.bottomCenter,
+                            end: Alignment.topCenter,
+                            colors: [
+                          Colors.black,
+                          Colors.black.withOpacity(0.1),
+                        ])),
                   ),
                 ),
                 Positioned(
-                  left: 10,
-                  bottom: 10,
-                  right: 10,
+                  left: 10.0,
+                  bottom: 10.0,
+                  right: 10.0,
                   child: Row(
                     mainAxisSize: MainAxisSize.max,
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -342,21 +344,32 @@ class CityCard extends StatelessWidget {
                             style: TextStyle(
                                 fontWeight: FontWeight.bold,
                                 color: Colors.white,
-                                fontSize: 18),
+                                fontSize: 18.0),
                           ),
                           Text(
                             monthYear,
                             style: TextStyle(
                                 fontWeight: FontWeight.normal,
                                 color: Colors.white,
-                                fontSize: 14),
+                                fontSize: 14.0),
                           ),
                         ],
                       ),
                       Container(
-                        padding: EdgeInsets.symmetric(horizontal: 6, vertical: 2),
-                        decoration: BoxDecoration(color: Colors.white, shape: BoxShape.rectangle, borderRadius: BorderRadius.all(Radius.circular(12))),
-                        child: Text("$discount%", style: TextStyle(fontSize: 14, color: Colors.black),))
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 6.0, vertical: 2.0),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          shape: BoxShape.rectangle,
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10.0),
+                          ),
+                        ),
+                        child: Text(
+                          "$discount%",
+                          style: TextStyle(fontSize: 14.0, color: Colors.black),
+                        ),
+                      ),
                     ],
                   ),
                 )
@@ -367,10 +380,26 @@ class CityCard extends StatelessWidget {
             mainAxisSize: MainAxisSize.max,
             mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
-              SizedBox(width: 5,),
-              Text('${formatCurrency.format(newPrice)}', style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 14,),),
-              SizedBox(width: 5,),
-              Text('${formatCurrency.format(oldPrice)}', style: TextStyle(color: Colors.grey, fontWeight: FontWeight.normal, fontSize: 12,),),
+              SizedBox(
+                width: 5.0,
+              ),
+              Text(
+                '${formatCurrency.format(newPrice)}',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 14.0),
+              ),
+              SizedBox(
+                width: 5.0,
+              ),
+              Text(
+                "(${formatCurrency.format(oldPrice)})",
+                style: TextStyle(
+                    color: Colors.grey,
+                    fontWeight: FontWeight.normal,
+                    fontSize: 12.0),
+              ),
             ],
           )
         ],
