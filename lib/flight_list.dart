0e1151582a7100b009c9099e7e624f6159ac36fb
Aby Abraham <abyabrkal@gmail.com>
2019-04-17 13:35:14 +0400

    Minor upgrades

--- lib/flight_list.dart
+++ lib/flight_list.dart
@@ -11,16 +11,15 @@ class FlightListingScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Search Results"),
-        elevation: 0,
-        centerTitle: true,
-        leading: InkWell(
-          child: Icon(Icons.arrow_back),
-          onTap: (){
-            Navigator.pop(context);
-          },  
-        )
-      ),
+          title: Text("Search Results"),
+          elevation: 0,
+          centerTitle: true,
+          leading: InkWell(
+            child: Icon(Icons.arrow_back),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          )),
       body: Column(
         children: <Widget>[
           FlightListTopPart(),
@@ -39,33 +38,58 @@ class FlightListTopPart extends StatelessWidget {
           clipper: CustomShapeClipper(),
           child: Container(
             decoration: BoxDecoration(
-              gradient: LinearGradient(colors: [firstColor, secondColor]),),
+              gradient: LinearGradient(colors: [firstColor, secondColor]),
+            ),
             height: 160,
           ),
         ),
         Column(
           children: <Widget>[
-            SizedBox(height: 20,),
+            SizedBox(
+              height: 20,
+            ),
             Card(
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(16))),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10))),
               margin: EdgeInsets.symmetric(horizontal: 16),
               elevation: 10,
               child: Container(
-                padding: EdgeInsets.all(16),
+                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 22),
                 child: Row(
                   mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text('Dubai (DXB)',style: TextStyle(fontSize: 16,),),
-                        Divider(color: Colors.grey, height: 20,),
-                        Text('New York City (JFK)',style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
-                      ],
+                    Expanded(
+                      flex: 5,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            'Dubai (DXB)',
+                            style: TextStyle(
+                              fontSize: 16,
+                            ),
+                          ),
+                          Divider(
+                            color: Colors.grey,
+                            height: 20,
+                          ),
+                          Text(
+                            'New York City (JFK)',
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
                     ),
-                    Icon(Icons.import_export, color: Colors.black,),
+                    Spacer(),
+                    Expanded(
+                        flex: 1,
+                        child: Icon(
+                          Icons.import_export,
+                          color: Colors.black,
+                          size: 30,
+                        )),
                   ],
                 ),
               ),
